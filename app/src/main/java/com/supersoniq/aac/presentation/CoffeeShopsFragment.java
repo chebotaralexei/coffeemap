ae346c6bce5c4d2daf045d4d1fd1f402bf07dbe1
achebotar <a.chebotar@um-agency.com>
2019-06-25 23:14:27 +0300

    add map

--- app/src/main/java/com/supersoniq/aac/presentation/CoffeeShopsFragment.java
+++ app/src/main/java/com/supersoniq/aac/presentation/CoffeeShopsFragment.java
@@ -13,11 +13,17 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.supersoniq.aac.R;
 
 import static com.supersoniq.aac.Utils.findById;
 
-public class CoffeeShopsFragment extends Fragment {
+public class CoffeeShopsFragment extends Fragment implements OnMapReadyCallback {
 
     @NonNull
     private CoffeeShopsViewModel model;
@@ -27,6 +33,7 @@ public class CoffeeShopsFragment extends Fragment {
     private RecyclerView coffeeShopsList;
     @NonNull
     private FrameLayout progress;
+    private GoogleMap mMap;
 
     @Nullable
     @Override
@@ -54,5 +61,23 @@ public class CoffeeShopsFragment extends Fragment {
         model.init();
 
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
